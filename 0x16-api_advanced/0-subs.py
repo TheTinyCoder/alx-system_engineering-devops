56794185273564600573222d9b974f02825cc18f
charawe <charawe.yw@gmail.com>
2024-06-19 05:01:13 +0300

    resolve

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,19 +17,14 @@ def number_of_subscribers(subreddit):
     Returns:
         int: number of subscribers
     """
-    base_url = 'https://www.reddit.com/r/'
+    if subreddit is None:
+        return (0)
 
-    url = '{}{}/about.json'.format(base_url, subreddit)
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"user-agent": 'API Project by yvonnec'}
     results = requests.get(
         url,
         headers=headers,
         allow_redirects=False
     )
-    if results.status_code == 200:
-        return results.json()['data']['subscribers']
-    return 0
+    return results.json()['data']['subscribers']
